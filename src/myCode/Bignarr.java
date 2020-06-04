2731f8945c5d2eafb664904562287ef5ff061c81
rakesh3b <rakesh.balugram@gmail.com>
2020-06-04 05:37:31 -0500

    Create Bignarr.java

---
+++ src/myCode/Bignarr.java
@@ -0,0 +1,30 @@
+package myCode;
+
+import java.util.*; 
+
+import java.io.*; 
+
+	class Bignarr 
+	{ 
+		 
+		static void arrUtil(int arr[], int n) 
+		{ 
+			int counter = 0; 
+			for (int i = 0; i < n; i++) 
+				if (arr[i] != 0) 
+					arr[counter++] = arr[i]; 
+			while (counter < n) 
+				arr[counter++] = 0; 
+		} 
+
+		
+		public static void main (String[] args) 
+		{ 
+			int intarr[] = {2, 3, 4, 0, 5, 0, 9, 3}; 
+			int n = intarr.length; 
+			arrUtil(intarr, n); 
+			System.out.println("Data "); 
+			for (int i=0; i<n; i++) 
+				System.out.print(intarr[i]+" "); 
+		} 
+	} 
